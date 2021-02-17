ffaf2442f0c22d2c8ab980675d1a6d64a23289bd
JCAustria <johncarloaustria@protonmail.com>
2021-02-17 13:53:02 +0800

    removed undefined type in media.ts

--- lib/media.ts
+++ lib/media.ts
@@ -1,6 +1,6 @@
 import { getURL } from './api';
 
-export function fetchMedia(media: { url: string | undefined; }) {
+export function fetchMedia(media: { url: string }) {
   const imageUrl = media?.url?.startsWith("/") ? getURL(media.url) : media.url
   return imageUrl;
 }
