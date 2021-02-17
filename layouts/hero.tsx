ff75a102c992c64403451cb887afb231cb53b908
JCAustria <johncarloaustria@protonmail.com>
2021-02-17 13:50:13 +0800

    Removed useMQ for the hero component

--- layouts/hero.tsx
+++ layouts/hero.tsx
@@ -1,31 +1,24 @@
-import styled from "styled-components";
-import * as U from "@/utils/display.sc";
-import * as SC from "./hero.sc";
-import { useMediaQuery } from "react-responsive";
+import styled from 'styled-components';
+import * as U from '@/utils/display.sc';
+import SC from './hero.sc';
 const Hero = () => {
-  const isDesktopOrLaptop = useMediaQuery({
-    minWidth: 1024,
-  });
   return (
     <>
-      <U.Grid cols={isDesktopOrLaptop ? 2 : 1} rows={1}>
-        <SC.Header>
-          <SC.HeaderName initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-            John Carlo Austria
-          </SC.HeaderName>
-          <SC.HeaderEN initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-            Frontend Developer
-          </SC.HeaderEN>
-          <SC.HeaderJP initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-            フロントエンドの開発者
-          </SC.HeaderJP>
-        </SC.Header>
-        <HeaderIcons>adwfasdfaffdf</HeaderIcons>
-      </U.Grid>
+      <SC.Header>
+        <SC.HeaderName initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          John Carlo Austria
+        </SC.HeaderName>
+        <SC.HeaderEN initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          Frontend Developer
+        </SC.HeaderEN>
+        <SC.HeaderJP initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          フロントエンドの開発者
+        </SC.HeaderJP>
+      </SC.Header>
     </>
   );
 };
-const HeaderIcons = styled("div")`
+const HeaderIcons = styled('div')`
   background-color: rgba(9, 143, 185, 1);
   grid-column: span 1 / span 1;
 `;
