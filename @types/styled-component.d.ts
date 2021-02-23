33d3a7b22c1fd1777e1120394ebfb8e007fbc1d5
JCAustria <johncarloaustria@protonmail.com>
2021-02-23 18:31:15 +0800

    TEST: Prop types

---
+++ @types/styled-component.d.ts
@@ -0,0 +1,30 @@
+export interface SCProps {
+  gridCols?: string;
+  fontWeight?: string;
+  fontSizeMin?: string;
+  fontSizeInitial?: string;
+  fontSizeMax?: string;
+  textAlign?: string;
+  display?: string;
+  widthVal?: string;
+  heightVal?: string;
+  maxWidthVal?: string;
+  maxHeightVal?: string;
+  padding?: string;
+  margin?: string;
+  mobile?: {
+    gridCols?: string;
+    fontWeight?: string;
+    fontSizeMin?: string;
+    fontSizeInitial?: string;
+    fontSizeMax?: string;
+    textAlign?: string;
+    display?: string;
+    widthVal?: string;
+    heightVal?: string;
+    maxWidthVal?: string;
+    maxHeightVal?: string;
+    padding?: string;
+    margin?: string;
+  };
+}
